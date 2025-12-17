fded3990487e7a31fb6425505af3bc66b69b706e
AstraAbigail <astrabigail@gmail.com>
2025-12-17 16:22:53 -0300

    mod estado.js

--- src/constants/estados.js
+++ src/constants/estados.js
@@ -1,6 +1,6 @@
 const ORDEN_ESTADOS = [
   { id: 1, value: "pendiente", content: "Pendiente de pago" },
-  { id: 2, value: "en preparación", content: "En preparación" },
+  { id: 2, value: "en preparacion", content: "En preparación" },
   { id: 3, value: "envio etiqueta", content: "Envío de etiqueta" },
   { id: 4, value: "despachado", content: "Despachado" },
   { id: 5, value: "entregado", content: "Entregado" },
