24f487710c8a1307877baea79886788553f32bde
AstraAbigail <astrabigail@gmail.com>
2025-12-17 16:17:18 -0300

    volvemos a la version anterior de estados.js

--- src/constants/estados.js
+++ src/constants/estados.js
@@ -1,12 +1,12 @@
 const ORDEN_ESTADOS = [
-  { id: 1, value: "Pendiente de pago", content: "Pendiente de pago" },
-  { id: 2, value: "En preparacion", content: "En preparación" },
-  { id: 3, value: "Envio etiqueta", content: "Envío de etiqueta" },
-  { id: 4, value: "Despachado", content: "Despachado" },
-  { id: 5, value: "Entregado", content: "Entregado" },
-  { id: 6, value: "Cancelado", content: "Cancelado" },
-  { id: 7, value: "Reembolso", content: "Reembolso" },
-  { id: 8, value: "Incidencias", content: "Incidencias" }
+  { id: 1, value: "pendiente de pago", content: "Pendiente de pago" },
+  { id: 2, value: "en preparacion", content: "En preparación" },
+  { id: 3, value: "envio etiqueta", content: "Envío de etiqueta" },
+  { id: 4, value: "despachado", content: "Despachado" },
+  { id: 5, value: "entregado", content: "Entregado" },
+  { id: 6, value: "cancelado", content: "Cancelado" },
+  { id: 7, value: "reembolso", content: "Reembolso" },
+  { id: 8, value: "incidencias", content: "Incidencias" }
 ];
 
 export { ORDEN_ESTADOS };
